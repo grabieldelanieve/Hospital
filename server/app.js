a95f5eabd56592d6cc3ad2a27b0d5af97763b7a4
Grabiel De La Nieve <GrabielDeLaNieve@users.noreply.github.com>
2020-04-15 16:28:00 -0400

    Refactoring

--- server/app.js
+++ server/app.js
@@ -23,17 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-mongoose.connection.openUri(
-  "mongodb://localhost:27017/hospitalDB",
-  { useNewUrlParser: true },
-  
-  (err, res) => {
-      if (err) throw err;
-      console.log("Base de datos: \x1b[32m%s\x1b[0m", "online");
-  }
-);
-
-// app = require('../routes/index');
+mongoose = require('../models/connectionDB');
 
 // Importar rutas
 var appRoutes = require('../routes/app');
